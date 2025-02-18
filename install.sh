1d4edb470005dd9d2af825714d61f029f7be5cbb
0000xFFFF <coldmailgg@gmail.com>
2025-02-18 11:10:11 +0100

    rename ; install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+sudo ln -sfr dcm_dminimap /usr/local/bin/.
+sudo ln -sfr dcm_dminimap_fs /usr/local/bin/.
+sudo ln -sfr dcm_minimap /usr/local/bin/.
+sudo ln -sfr dcm_rect /usr/local/bin/.
+sudo ln -sfr dcm_rect_zoom /usr/local/bin/.
+sudo ln -sfr dcm_zoom /usr/local/bin/.
