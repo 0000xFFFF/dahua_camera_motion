5130eb88592f896220be7fe029fda188efe89c53
0000xFFFF <coldmailgg@gmail.com>
2025-02-20 13:22:05 +0100

    new install

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 sudo ln -sfr dcm_dminimap /usr/local/bin/.
-sudo ln -sfr dcm_dminimap_fs /usr/local/bin/.
-sudo ln -sfr dcm_minimap /usr/local/bin/.
+sudo ln -sfr dcm_hd /usr/local/bin/.
+sudo ln -sfr dcm_hd_ffmpeg /usr/local/bin/.
 sudo ln -sfr dcm_rect /usr/local/bin/.
-sudo ln -sfr dcm_rect_zoom /usr/local/bin/.
-sudo ln -sfr dcm_zoom /usr/local/bin/.
