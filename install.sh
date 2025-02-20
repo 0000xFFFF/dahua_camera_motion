2c8dbb80fbb219c06395e2d901661d56740a1975
0000xFFFF <coldmailgg@gmail.com>
2025-02-20 15:13:25 +0100

    needs more testing

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 sudo ln -sfr dcm_dminimap /usr/local/bin/.
 sudo ln -sfr dcm_rect /usr/local/bin/.
 sudo ln -sfr dcm_master /usr/local/bin/.
+sudo ln -sfr dcm_hd /usr/local/bin/.
+sudo ln -sfr dcm_hd_ffmpeg /usr/local/bin/.
