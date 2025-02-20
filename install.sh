06f569182adda29fb4ff12bb9a7a5dfccb53a257
0000xFFFF <coldmailgg@gmail.com>
2025-02-20 15:31:29 +0100

    install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 sudo ln -sfr dcm_dminimap /usr/local/bin/.
 sudo ln -sfr dcm_rect /usr/local/bin/.
-sudo ln -sfr dcm_master /usr/local/bin/.
 sudo ln -sfr dcm_hd /usr/local/bin/.
 sudo ln -sfr dcm_hd_ffmpeg /usr/local/bin/.
+sudo ln -sfr dcm_hd_ffmpeg_threaded /usr/local/bin/.
