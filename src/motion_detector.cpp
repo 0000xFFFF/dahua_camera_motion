0075a1656f96507dfb924f6195fe7e52479936ae
0000xFFFF <coldmailgg@gmail.com>
2025-03-26 02:56:05 +0100

    rm imshow

--- src/motion_detector.cpp
+++ src/motion_detector.cpp
@@ -50,11 +50,9 @@ std::vector<std::vector<cv::Point>> MotionDetector::find_contours_frame0()
 {
     cv::Mat fgmask;
     m_fgbg->apply(m_frame0, fgmask);
-    cv::imshow("fgmask", fgmask);
 
     cv::Mat thresh;
     cv::threshold(fgmask, thresh, 128, 255, cv::THRESH_BINARY);
-    cv::imshow("thresh", thresh);
 
     std::vector<std::vector<cv::Point>> contours;
     cv::findContours(thresh, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
